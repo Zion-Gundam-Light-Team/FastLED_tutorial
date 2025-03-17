e6b0539f2487b704a4a6fe5d609297f0c7d10201
AngelaTSE <>
2025-03-17 18:39:47 +0800

    remove FastLED, add monitor port

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,18 @@
 #include <Arduino.h>
-#include <FastLED.h>
 
-#define LED_PIN_RGB1 4
+#define LED_PIN_4 4
 #define LED_PIN_5 5
 #define LED_PIN_6 6
-
-#define NUM_LEDS_RGB1 64
-
-CRGB leds_RGB1[NUM_LEDS_RGB1];
 
 void setup()
 {
     Serial.begin(115200);
-    FastLED.addLeds<WS2812B, LED_PIN_RGB1, GRB>(leds_RGB1, NUM_LEDS_RGB1);
-    FastLED.clear();
-    FastLED.setBrightness(40);
+    pinMode(LED_PIN_4, OUTPUT);
     pinMode(LED_PIN_5, OUTPUT);
     pinMode(LED_PIN_6, OUTPUT);
 }
 void loop()
 {
+    Serial.println("hello world");
+    delay(1000);
 }
