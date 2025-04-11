2559933a2444800639563e60eed92cb15a07e984
AngelaTSE <>
2025-04-11 14:48:03 +0800

    update README

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,8 @@
 #include <Arduino.h>
 
-#define LED_PIN_4 4
-#define LED_PIN_5 5
-#define LED_PIN_6 6
-
 void setup()
 {
     Serial.begin(115200);
-    pinMode(LED_PIN_4, OUTPUT);
-    pinMode(LED_PIN_5, OUTPUT);
-    pinMode(LED_PIN_6, OUTPUT);
 }
 void loop()
-{
-    Serial.println("hello world");
-    delay(1000);
-}
+{}
