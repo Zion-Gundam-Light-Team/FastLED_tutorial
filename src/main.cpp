4b416b862f25f1b30ed679e6f50b92142155bba9
honest4794 <tty629660@gmail.com>
2026-03-30 20:40:41 +0800

    example of led_random_flash

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,42 @@
 #include <Arduino.h>
+#include <FastLED.h>
 
-void setup()
-{
+uint8_t randomChance2 = 80;
+int interval2 = 100;
+int brightnessLow2 = 0;
+int brightnessHigh2 = 100;
+unsigned long onDuration2 = 10;
+
+void setup() {
     Serial.begin(115200);
+
+    for (int i = 9; i <= 13; i++) {
+        pinMode(i, OUTPUT);
+    }
 }
-void loop()
-{}
+
+void led_random_flash(int pin, int interval, uint8_t randomChance,
+                      int brightnessLow, int brightnessHigh,
+                      unsigned long onDuration) {
+    if (random8() < randomChance) {  // 40/255 = 15%
+        analogWrite(pin, brightnessHigh);
+        delay(onDuration);  // Flash for 10ms
+        analogWrite(pin, brightnessLow);
+    }
+    delay(interval);
+}
+
+void loop() {
+    for (int i = 9; i <= 13; i++) {
+        if (random8() < 40) {  // random8() choose 0-39,
+            analogWrite(i, 100);
+            delay(10);  // Flash for 10ms
+            analogWrite(i, 0);
+        }
+    }
+    delay(100);  // Interval = 100ms
+    // for (int i = 9; i <= 13; i++) {
+    //     led_random_flash(i, interval2, randomChance2, brightnessLow2,
+    //                      brightnessHigh2, onDuration2);
+    // }
+}
