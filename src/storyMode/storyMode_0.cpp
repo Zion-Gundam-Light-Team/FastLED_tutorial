dacef1fadd85fd770d22f65921a8f831ab158da5
laptop\hi <0527samuel@gmail.com>
2026-04-11 18:24:07 +0800

    test comet

--- src/storyMode/storyMode_0.cpp
+++ src/storyMode/storyMode_0.cpp
@@ -112,6 +112,8 @@ int freq = 20;
 uint8_t brightnessLow = 1;
 uint8_t brightnessHigh = 50;
 unsigned long lastUpdate = 0;
+    uint8_t lastPos = 255;
+    bool reachedEnd = false;
 
 bool storyMode_0()
 {
@@ -127,10 +129,16 @@ bool storyMode_0()
         mode0State = MODE_0_MAIN;
         return false;
     case MODE_0_MAIN:
-        paletteFlow(leds_RGB1, blueWaveIndex_RGB1, NUM_RGB1, &lastUpdate_blueWave1, &blueWaveInit1, blue_wave_p, 1);
-        paletteFlow(leds_RGB2, blueWaveIndex_RGB2, NUM_RGB2, &lastUpdate_blueWave2, &blueWaveInit2, blue_wave_p, 1);
-        paletteFlow(leds_RGB3, blueWaveIndex_RGB3, NUM_RGB3, &lastUpdate_blueWave3, &blueWaveInit3, blue_wave_p, 1);
+
+
+    comet(leds_RGB1, 10, CRGB::AntiqueWhite, 30, 2, 2, &lastPos, &reachedEnd, false);
+
+
+        // paletteFlow(leds_RGB1, blueWaveIndex_RGB1, NUM_RGB1, &lastUpdate_blueWave1, &blueWaveInit1, blue_wave_p, 1);
+        // paletteFlow(leds_RGB2, blueWaveIndex_RGB2, NUM_RGB2, &lastUpdate_blueWave2, &blueWaveInit2, blue_wave_p, 1);
+        // paletteFlow(leds_RGB3, blueWaveIndex_RGB3, NUM_RGB3, &lastUpdate_blueWave3, &blueWaveInit3, blue_wave_p, 1);
         return false;
+
     case MODE_0_END:
         rgbOff(leds_RGB1, NUM_RGB1);
         pwmOffAll(pwmBuffer);
